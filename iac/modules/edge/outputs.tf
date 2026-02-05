f6af2a39fa63f9629b92af345af04c06298bc409
WT0715 <cloudshell-user@ip-10-143-116-159.us-east-1.compute.internal>
2026-02-05 17:42:16 +0000

    update

--- iac/modules/edge/outputs.tf
+++ iac/modules/edge/outputs.tf
@@ -3,3 +3,4 @@ output "alb_dns_name" { value = aws_lb.this.dns_name }
 output "target_group_arn" { value = aws_lb_target_group.app.arn }
 output "http_listener_arn" { value = aws_lb_listener.http.arn }
 output "https_listener_arn" { value = try(aws_lb_listener.https[0].arn, null) }
+output "alb_arn_suffix" { value = aws_lb.this.arn_suffix }
