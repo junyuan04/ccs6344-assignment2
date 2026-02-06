d903b9a6cfa67dac33684d5998fb67e47cef58ba
Woon Wen Tao <1211108861@student.mmu.edu.my>
2026-02-06 09:19:44 +0800

    add http at security main

--- iac/modules/security/main.tf
+++ iac/modules/security/main.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+  description = "HTTP"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
   egress {
     from_port   = 0
     to_port     = 0
