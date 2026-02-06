773cff138b2773143b82d29a76c3a38e8734cea1
Woon Wen Tao <1211108861@student.mmu.edu.my>
2026-02-06 09:46:29 +0800

    port 5000

--- iac/modules/security/main.tf
+++ iac/modules/security/main.tf
@@ -33,8 +33,8 @@ resource "aws_security_group" "app_sg" {
   description = "App SG - allow traffic from ALB"
 
   ingress {
-    from_port       = 3000
-    to_port         = 3000
+    from_port       = 5000
+    to_port         = 5000
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
     description     = "Allow ALB"
