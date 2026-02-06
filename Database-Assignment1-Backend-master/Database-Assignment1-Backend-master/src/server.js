4e29b31f986cc1fcd7ebb9163ab25842ed298d63
Woon Wen Tao <1211108861@student.mmu.edu.my>
2026-02-06 11:30:27 +0800

    port5000

--- Database-Assignment1-Backend-master/Database-Assignment1-Backend-master/src/server.js
+++ Database-Assignment1-Backend-master/Database-Assignment1-Backend-master/src/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Import all routes
 const authRoutes = require('./routes/auth');
